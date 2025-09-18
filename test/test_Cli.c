04eda7b187f0b6041fb2fee4bee65d16aa282394
maxkoell <maxkoell@proton.me>
2025-09-18 20:11:52 +0200

    There is no shell, but only a cli ... muahaha

---
+++ test/test_Cli.c
@@ -0,0 +1,21 @@
+
+#ifdef TEST
+
+#include "unity.h"
+
+#include "Cli.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_Cli_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement Cli");
+}
+
+#endif // TEST
