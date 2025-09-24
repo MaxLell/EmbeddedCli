d6a9784742bebe02ae301b42e601ed8786e91e92
Your Name <maximilian.lell@gmail.com>
2025-09-24 18:57:13 +0200

    Major Rework on adding callbacks to the CLI - not yet working!!

--- main.c
+++ main.c
@@ -18,30 +18,27 @@ char Console_GetCharacter( void )
     return (char)getchar();
 }
 
+static Cli_Config_t  tCliCfg = { 0 };
+static Cli_Binding_t atCliBindings[] = {
+    { "hello", Cli_HelloWorld, "Say hello" },
+    { "help", Cli_HelpCommand, "Lists all commands" },
+    { "display_args", Cli_DisplayArgs, "Displays the given cli arguments" },
+    { "clear", Cli_ClearScreen, "Clears the screen" },
+    { "echo", Cli_EchoString, "Echoes the given string" },
+};
+
 int main( void )
 {
-    Cli_Binding_t atCliBindings[] = {
-        { "hello", Cli_HelloWorld, "Say hello" },
-        { "help", Cli_HelpCommand, "Lists all commands" },
-        { "display_args", Cli_DisplayArgs, "Displays the given cli arguments" },
-        { "clear", Cli_ClearScreen, "Clears the screen" },
-        { "echo", Cli_EchoString, "Echoes the given string" },
-    };
 
-    Cli_Config_t tCliCfg = {
-        .pFnWriteCharacter = Console_PutCharacter,
-        .bIsInitialized = false,
-        .atCliCmdBindingsBuffer = atCliBindings,
-        .tNofBindings = CLI_GET_ARRAY_SIZE( atCliBindings ),
-    };
 
-    Cli_Initialize( &tCliCfg );
+    Cli_Init( &tCliCfg, atCliBindings, CLI_GET_ARRAY_SIZE( atCliBindings ),
+              Console_PutCharacter );
 
     while( 1 )
     {
         char c = Console_GetCharacter();
         Cli_AddCharacter( &tCliCfg, c );
-        Cli_Process( &tCliCfg );
+        Cli_ProcessBuffer( &tCliCfg );
     }
     return 0;
 }
